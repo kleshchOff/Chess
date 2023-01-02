163f03d57337d03a397824b4c9d67cc9e9b6942e
Nikita <kleshch.off@gmail.com>
2023-01-02 16:24:02 +0200

    figures done

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,41 @@
+import {Colors} from "../Colors";
+import logo from "../../assets/black-king.png"
+import {Cell} from "../Cell";
+
+export enum FigureNames {
+    FIGURE = "Figure",
+    KING = "King",
+    KNIGHT = "Knight",
+    QUEEN = "Queen",
+    BISHOP = "Bishop",
+    ROOK = "Rook",
+    PAWN = "Pawn",
+
+}
+
 export class Figure {
 
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number
+
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random()
+    }
+
+    canMove(target: Cell): boolean {
+        return true;
+    }
+
+    moveFigure(target: Cell) {
+
+    }
 }
