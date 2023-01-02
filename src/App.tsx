6896bfe8e7f7e45788916283a91fe5a7734d201c
Nikita <klesch212002@gmail.com>
2023-01-02 14:22:48 +0200

    structure update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "App.css"
+import BoardComponent from "./components/BoardComponent";
+
+const App = () =>  {
+  return (
+      <div className="app">
+          <BoardComponent/>
+      </div>
+  );
+}
+
+export default App;
