e04c65c804b3c8f81fc9346b75812ab9b1123dd4
Nikita <kleshch.off@gmail.com>
2023-01-02 15:46:48 +0200

    board and cell fit

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -9,14 +9,14 @@ export class Board {
         for (let i = 0; i < 8; i++) {
             const row: Cell[] = []
             for (let j = 0; j < 8; j++) {
-                if ((i+j) %2 !== 0) {
+                if ((i+j) % 2 !== 0) {
                     row.push(new Cell(this, j, i, Colors.BLACK, null)) //black
                 }
                 else {
                     row.push(new Cell(this, j, i, Colors.WHITE, null)) //white
                 }
-                this.cells.push(row)
             }
+            this.cells.push(row)
         }
     }
 
